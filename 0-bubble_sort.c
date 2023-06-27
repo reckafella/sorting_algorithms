d42783d2920e95e26ba04466db6f4a2a76904026
reckafella <ethanmuthoniw6@gmail.com>
2023-06-27 17:03:45 +0300

    add 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - 1 - i); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# sorting_algorithms
-0x1B. C - Sorting algorithms &amp; Big O
+# Sorting Algorithms
+## 0x1B. C - Sorting algorithms & Big O
