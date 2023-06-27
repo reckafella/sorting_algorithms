f3b93954b19ef6bd8f9c38b6751faf9e572fb732
reckafella <ethanmuthoniw6@gmail.com>
2023-06-27 17:39:36 +0300

    update 0-bubble_sort.c file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,19 +11,22 @@ void bubble_sort(int *array, size_t size)
 	unsigned long int i, j;
 	int temp;
 
-	if (size >= 2)
+	if (!array || size < 2)
 	{
-		for (i = 0; i < size; i++)
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - 1 - i); j++)
 		{
-			for (j = 0; j < (size - 1 - i); j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
-					print_array(array, size);
-				}
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+				print_array(array, size);
 			}
 		}
 	}
